2a41da71fa8371be8d26b7a808723a19e3e561ee
Master <a.vladislavovnaa@mail.ru>
2020-04-06 13:23:01 +0500

    модели

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Poll(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll,on_delete=models.CASCADE)
+    text = models.TextField()
