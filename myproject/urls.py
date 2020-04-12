327bad21e3d7046bf9211baf7b9a6c5822708dcc
Евгения <e.zhilinkova@yandex.ru>
2020-04-12 23:18:15 +0500

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from pulls.views import PollViewSet
+from polls.views import PollViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
