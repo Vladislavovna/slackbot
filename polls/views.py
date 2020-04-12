b66dbf2296c9cf012c0e2fec6d697554ab22a9de
Евгения <e.zhilinkova@yandex.ru>
2020-04-12 22:00:57 +0500

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
 
-# Create your views here.
+from .models import Pull, Question
+
+class PullSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pull
+        fields = '___all___'
+
+class PullViewSet(viewsets.ModelViewSet):
+    serializer_class = PullSerializer
+    queryset = Pull.objects.all()
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '___all___'
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
