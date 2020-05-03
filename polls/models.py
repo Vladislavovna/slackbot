0128c6bf94f2cb0899133c452d5ee08b83731c93
Master <a.vladislavovnaa@mail.ru>
2020-05-03 14:21:22 +0500

    добавила SlackUser

--- polls/models.py
+++ polls/models.py
@@ -9,3 +9,7 @@ class Poll(models.Model):
 class Question(models.Model):
     poll = models.ForeignKey(Poll,on_delete=models.CASCADE)
     text = models.TextField()
+
+class SlackUser(models.Model):
+    name =  models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
