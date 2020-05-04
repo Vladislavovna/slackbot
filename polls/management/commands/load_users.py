1af28f70e0d83ce2bbf431fd2cda6ad28b446710
Master <a.vladislavovnaa@mail.ru>
2020-05-04 23:54:46 +0500

    Получение пользаков из Slack

--- polls/management/commands/load_users.py
+++ polls/management/commands/load_users.py
@@ -1,8 +1,23 @@
+import os
+import json
+from polls.models import SlackUser
+from slack import WebClient
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = 'Подгружает список пользователей'
 
+    
     def handle(self, *args, **options):
-        print('Я команда, вызываемая с помощью python manage.py load_users')
+        client = WebClient(token='xoxp-1093552724881-1080926571618-1100616862002-94586771c34453bfd2dd0d5090f33080')
+        data = client.users_list()
+
+        for member in data['members']:
+            if 'email' in member['profile']:
+                email = member['profile']['email']
+                name = member['profile']['real_name']
+                slack_id = member['id']
+                SlackUser.objects.get_or_create( username=name, email=email, slack_id = slack_id)
+
+     
