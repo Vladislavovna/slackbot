dd75ecfa41fe3fc2a0a2e2bacfdfaf8467f0f5e9
Евгения <e.zhilinkova@yandex.ru>
2020-05-17 18:10:43 +0500

    Update process_schedule.py

--- polls/management/commands/process_schedule.py
+++ polls/management/commands/process_schedule.py
@@ -1,8 +1,16 @@
 from django.core.management.base import BaseCommand
+from datetime import datetime
+from myproject import settings
+from polls import models
+from slack import WebClient
+from polls.models import PollSchedule
 
 
 class Command(BaseCommand):
     help = 'Запускает опрос'
 
     def handle(self, *args, **options):
-        print('Я команда, вызываемая с помощью python manage.py process_schedule')
+        tasks = PollSchedule.objects.filter(start_at__lte=datetime.now())
+        for task in tasks:
+            task.poll.send_poll_starter_to_user(task.slack_user)
+            task.delete()
